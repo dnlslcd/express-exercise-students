0a533d36ee8db139054ab98dc8e6b46a88c096d5
dnlslcd <danielsalcedoq@gmail.com>
2024-07-02 19:14:24 +0200

    server2 - hecho

--- server2.js
+++ server2.js
@@ -2,13 +2,14 @@ const express = require('express')
 
 const app = express()
 
+app.get('/mensaje', (req,res)=>{
+  res.send('mensaje');
+});
 
-
-app.use((req, res) => {
+app.use('*', (req, res) => {
     res.status(404).send('Recurso no encontrado...')
   })
 
 app.listen(3000, (req, res)=>{
-  res.send(`<h1>El servidor funciona correctamente</h1> 
-            <p>Aquí iría el contenido del mensaje</p>`)
+  console.log('Servidor corriendo en el puerto 3000')
 });
